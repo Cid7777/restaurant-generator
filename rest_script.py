70a76c7b31a689a3bf3ed08cf76d49a44cfb6dbc
Garrett Williams <garrett.williams58@gmail.com>
2024-07-10 15:28:55 +0000

    first commit

---
+++ rest_script.py
@@ -0,0 +1,5 @@
+from restaurant_gen import *
+
+rest_list = gen_restaurant_list()
+
+print(get_restaurant(rest_list))
---
+++ restaurant_gen.py
@@ -0,0 +1,15 @@
+import random
+
+def gen_restaurant_list():
+    rest_dir = "./restaurants/"
+    f = str(input("List the city:\n").lower() + "_rest.txt")
+
+    with open(rest_dir + f, "r") as file:
+        line_list = file.readlines()
+        rest_list = [item.rstrip() for item in line_list]
+    return rest_list
+
+def get_restaurant(list):
+
+    rand_num = random.randint(0, len(list)-1)
+    return f"The restaurant we'll be going to is: {list[rand_num]}"
